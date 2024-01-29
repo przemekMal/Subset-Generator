6855b7563c83561acc04ab0aa62025a5fa612803
Mr.PMal <126407292+przemekMal@users.noreply.github.com>
2024-01-29 12:06:31 +0100

    Update find_max_element_not_in_A.m

--- find_max_element_not_in_A.m
+++ find_max_element_not_in_A.m
@@ -1,3 +1,4 @@
+% This function finds the maximum element in the set `zbior` that is not present in the subset `A`.
 function max_element = find_max_element_not_in_A(zbior, A)
     max_element = -inf;  % Set the initial value to negative infinity
     for i = 1:length(zbior)
