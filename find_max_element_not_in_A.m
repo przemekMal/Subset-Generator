81dfa18f5b69db21aade44b1245e1ee8f2e48738
Mr.PMal <126407292+przemekMal@users.noreply.github.com>
2024-01-29 11:59:27 +0100

    Update and rename znajdz_max_ele_zbioru_bez_A.m to find_max_element_not_in_A.m

--- find_max_element_not_in_A.m
+++ find_max_element_not_in_A.m
@@ -1,5 +1,5 @@
-function max_element = znajdz_max_ele_zbioru_bez_A(zbior, A)
-    max_element = -inf;  % Ustaw początkową wartość na minus nieskończoność
+function max_element = find_max_element_not_in_A(zbior, A)
+    max_element = -inf;  % Set the initial value to negative infinity
     for i = 1:length(zbior)
         if ~ismember(zbior(i), A) && zbior(i) > max_element
             max_element = zbior(i);
