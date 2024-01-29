05310e1602d50c214d5bbb4a3af1756985fb4185
Mr.PMal <126407292+przemekMal@users.noreply.github.com>
2024-01-29 11:39:38 +0100

    Update and rename podzbior.m to subset.m

--- subset.m
+++ subset.m
@@ -1,3 +1,4 @@
+%The function creates a new subset by adding element n (if it is not -inf), removes elements greater than n, and returns the new subset along with the success status of the operation.
 function [A, sukces] = subset(n, X)
     A = X;
     if n ~= -inf
